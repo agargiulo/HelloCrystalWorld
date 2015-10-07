daeb7ddc409d8e41bf6cdfe1908abe5bd8f33a01
Anthony Gargiulo <anthony@agargiulo.com>
2015-10-06 22:40:53 -0400

    some fun http server thing

--- src/HelloCrystalWorld.cr
+++ src/HelloCrystalWorld.cr
@@ -1,5 +1,15 @@
 require "./HelloCrystalWorld/*"
+require "http/server"
 
 module HelloCrystalWorld
-  # TODO Put your code here
+  host, port = ARGV.first.split(":")
+  puts "This is version #{VERSION}"
+  server = HTTP::Server.new(host, port) do |request|
+    puts "#{request.method} #{request.version}"
+    puts request.headers
+    puts "Body: #{request.body}"
+    HTTP::Response.ok "text/plain", "Hello, world!"
+  end
+  puts "Listeing on http://#{host}:#{port}"
+  server.listen
 end
