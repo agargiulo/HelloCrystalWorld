48259c6517fe45cc7019d16f55cca82348198538
Anthony Gargiulo <anthony@agargiulo.com>
2015-10-07 18:10:08 -0400

    will figure out how make the test and binary part happy in a bit

--- src/HelloCrystalWorld.cr
+++ src/HelloCrystalWorld.cr
@@ -1,10 +1,12 @@
 require "./HelloCrystalWorld/*"
 
 module HelloCrystalWorld
-  host, port = ARGV.first.split(":")
-  puts "This is version #{VERSION}"
+  def main
+    host, port = ARGV.first.split(":")
+    puts "This is version #{VERSION}"
 
-  server = Server.new(host, port)
-  puts "Listeing on http://#{host}:#{port}"
-  server.listen
+    server = Server.new(host, port)
+    puts "Listeing on http://#{host}:#{port}"
+    server.listen
+  end
 end
