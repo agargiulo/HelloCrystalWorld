d71d44a66ae5f6b656617111c473f76e30c03121
Anthony Gargiulo <anthony@agargiulo.com>
2015-10-15 16:34:08 -0400

    I'm playing around with curl and HTTP, because I felt like

--- src/HelloCrystalWorld/server.cr
+++ src/HelloCrystalWorld/server.cr
@@ -14,10 +14,17 @@ module HelloCrystalWorld
     def process(client)
       client_addr = "#{client.peeraddr.ip_address}:#{client.peeraddr.ip_port}"
       puts "#{client_addr} connected".colorize(:magenta)
-      while msg = client.read_line
-        puts "#{client_addr} msg '#{msg.chop}'".colorize(:light_cyan)
-        client << msg
+      resp = ""
+      while resq = client.read_line
+        break if resq.chop == ""
+        puts "#{client_addr} msg '#{resq.chop}'".colorize(:light_cyan)
+        resp += resq
       end
+      header = "HTTP/1.1 200 OK
+Content-Length: #{resp.size}\r
+Content-Type: text/html\r"
+
+      client << "#{header}\r\n\r\n#{resp}\r\n"
 
     rescue IO::EOFError
       puts "#{client_addr} dissconnected".colorize(:magenta)
